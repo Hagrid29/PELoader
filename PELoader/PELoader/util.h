8f7839cc9474b2da70d4567329f17b8ac36dda80
Hagrid29 <97426612+Hagrid29@users.noreply.github.com>
2022-09-21 23:56:09 +0800

    bug fixed

--- PELoader/PELoader/util.h
+++ PELoader/PELoader/util.h
@@ -4,7 +4,7 @@
 DWORD translate_protect(DWORD sec_charact);
 bool is_compatibile(BYTE *implant_dll);
 
-void encryptFile(const char* payloadPath);
+void encryptFile(const char* payloadPath, char* outputPath);
 BYTE* decryptFile(const char* payloadPath, OUT size_t& r_size);
 
 
